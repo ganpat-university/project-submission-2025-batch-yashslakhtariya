f2a0fc38b5d78a64bc77b48064f476f2d1b76f31
Yash S Lakhtariya <98678102+yashslakhtariya@users.noreply.github.com>
2025-05-04 20:31:56 +0530

    Added IBM Project Folder

---
+++ rapidlekh-master/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    geminiApiKey: 'AIzaSyDEim-JrcOdafWV5hZXra_JF5nFSBPo2do',
+    groqApiKey1: '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',
+    groqApiKey2: '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'
+};
